6ac751f26f1e99168bc5f523afa4c1d27aadb41d
MurtuzaBukhari <137167224+MurtuzaBukhari@users.noreply.github.com>
2023-10-10 19:08:15 +0530

    Save

---
+++ force-app/main/default/aura/auraApp6/auraApp6.app
@@ -0,0 +1,3 @@
+<aura:application>
+<c:imageUpload_Aura_6/>
+</aura:application>	
---
+++ force-app/main/default/aura/imageUpload_Aura_6/imageUpload_Aura_6Controller.js
@@ -0,0 +1,16 @@
+({
+    myAction: function (component, event, helper) {
+        // var color = component.get("backgroundColor");
+        // var color = component.get("v.backgroundColor");
+        // var fontSize = component.get("v.fontsize");
+        // var fontColor = component.get("v.fontColor");
+        // console.log("Color"+color);
+        // console.log("size"+fontSize);
+        // console.log("fcolor"+fontColor);
+
+        // component.getElement().style.setProperty("—background-color", color);
+        // component.getElement().style.setProperty("—font-size", fontSize);
+        // component.getElement().style.setProperty("—font-color", fontColor);
+
+    }
+})
