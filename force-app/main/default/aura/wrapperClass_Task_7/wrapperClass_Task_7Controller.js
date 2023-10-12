e5b49c46c9fdbed383d7d4ab28ce3e38bcb2020c
MurtuzaBukhari <137167224+MurtuzaBukhari@users.noreply.github.com>
2023-10-12 18:38:38 +0530

    Save

--- force-app/main/default/aura/wrapperClass_Task_7/wrapperClass_Task_7Controller.js
+++ force-app/main/default/aura/wrapperClass_Task_7/wrapperClass_Task_7Controller.js
@@ -5,7 +5,12 @@
             var state = response.getState();
             if (state == "SUCCESS") {
                 var accList = response.getReturnValue();
+                console.log("List",accList);
                 component.set("v.wrapperClass",accList)
+                accList.forEach(list => {
+                    // console.log(list.objcon);
+                });
+                // console.log("Contact : ",accList[8].objcon[4]);
             }
         });
         $A.enqueueAction(create);
