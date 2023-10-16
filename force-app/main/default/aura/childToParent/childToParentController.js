0b13817e979dbeeacf94d2b7fca960ae39ac15d9
MurtuzaBukhari <137167224+MurtuzaBukhari@users.noreply.github.com>
2023-10-16 12:10:14 +0530

    save

--- force-app/main/default/aura/childToParent/childToParentController.js
+++ force-app/main/default/aura/childToParent/childToParentController.js
@@ -2,15 +2,14 @@
     save: function (component, event, helper) {
         var file = component.find("fileId").get("v.files");
         var Upload = file[0];
-        console.log("Files : ",Upload);
+        console.log("Files : ", Upload);
         var reader = new FileReader();
         console.log("Hello");
-        reader.onload = function() {
+        reader.onload = function () {
             var fileUp = reader.result;
-            component.set("v.image",fileUp);
+            component.set("v.image", fileUp);
             console.log("Images" + fileUp);
         }
-        reader.readAsDataURL(Upload); 
+        reader.readAsDataURL(Upload);
     },
-    
 })
